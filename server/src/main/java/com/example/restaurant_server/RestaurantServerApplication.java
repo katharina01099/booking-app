824e5db6779f0e3b71324cc78192e632f9f20152
Slawomir Dyk <slawomirdyk@gmail.com>
2019-03-08 13:14:08 +0000

    Blank java project

---
+++ server/src/main/java/com/example/restaurant_server/RestaurantServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantServerApplication.class, args);
+	}
+
+}
