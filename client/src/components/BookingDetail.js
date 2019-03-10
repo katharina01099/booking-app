92afaee3a926eb050660dce91efc816d29bd1d5b
Michael Irwin <mike.irwin1@gmail.com>
2019-03-10 11:13:19 +0000

    refactored state, pulled in data from back end and displayed on front end

--- client/src/components/BookingDetail.js
+++ client/src/components/BookingDetail.js
@@ -3,7 +3,10 @@ import React from "react";
 const BookingDetail = (props) => {
     return (
         <div className="booking-detail">
-            <h5>layer 5</h5>
+            <li>{props.date}</li>
+            <li>{props.time}</li>
+            <li>{props.numPeople}</li>
+            <hr></hr>
         </div>
     )
 }
