df1b2eed66edee6a5898f87b913db83bad441e68
Thomas Kendrick <tommkendrick@gmail.com>
2019-03-11 10:50:38 +0000

    some css

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className = "window">
-        <h1>Window layer 1</h1>
+        <h1 id ='appheader'>A wonderful booking app.</h1>
         <PageContainer/>
       </div>
     );
