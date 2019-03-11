1aa97aebbafd2f3152336f8f6e96cc44fd1d29ee
Thomas Kendrick <tommkendrick@gmail.com>
2019-03-11 20:57:36 +0000

    Cleanup

--- client/src/components/BookingTableForm.js
+++ client/src/components/BookingTableForm.js
@@ -1,78 +1,76 @@
-import React, {Component} from 'react';
-import "./BookingTableForm.css"
+import React, { Component } from 'react';
+import './BookingTableForm.css';
 class BookingTableForm extends Component {
-
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-    date: '',
-    time: '',
-    numPeople: ''
-    }
+      name: '',
+      date: '',
+      time: '',
+      numPeople: ''
+    };
 
-    this.handleDateChange = this.handleDateChange.bind(this)
-    this.handleTimeChange = this.handleTimeChange.bind(this)
-    this.handleNumOfPeopleChange = this.handleNumOfPeopleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-}
-    handleDateChange(e){
-      this.setState({date: e.target.value})
-    }
-
-    handleTimeChange(e){
-      this.setState({time: e.target.value})
-    }
-
-    handleNumOfPeopleChange(e){
-      this.setState({numPeople: e.target.value})
-    }
-
-    handleSubmit(e){
-    e.preventDefault();
-    const newBookingTable = {date: this.state.date, time: this.state.time, numPeople: this.state.numPeople}
-    this.props.onBookingTableSubmit(newBookingTable)
-    this.setState({date: '', time: '', numPeople: ''})
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleTimeChange = this.handleTimeChange.bind(this);
+    this.handleNumOfPeopleChange = this.handleNumOfPeopleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleDateChange(e) {
+    this.setState({ date: e.target.value });
   }
 
+  handleTimeChange(e) {
+    this.setState({ time: e.target.value });
+  }
 
+  handleNumOfPeopleChange(e) {
+    this.setState({ numPeople: e.target.value });
+  }
 
-render(){
-  return(
+  handleSubmit(e) {
+    e.preventDefault();
+    const newBookingTable = {
+      date: this.state.date,
+      time: this.state.time,
+      numPeople: this.state.numPeople
+    };
+    this.props.onBookingTableSubmit(newBookingTable);
+    this.setState({ date: '', time: '', numPeople: '' });
+  }
 
-    <form
-        className="booking-table-form"
-        onSubmit={this.handleSubmit}
-      >
-    <input
-    type="text"
-    placeholder="Date of booking"
-    value={this.state.date}
-    onChange={this.handleDateChange}
-    />
-
-    <input
-    type="text"
-    placeholder="Time of booking"
-    value={this.state.time}
-    onChange={this.handleTimeChange}
-/>
-    <input
-    type="text"
-    placeholder="Number of people booking"
-    value={this.state.numPeople}
-    onChange={this.handleNumOfPeopleChange}
-
-    />
-
-    <input
-          type="submit"
-          value="Post"
+  render() {
+    return (
+      <form className="booking-table-form" onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={this.state.date}
+          onChange={this.handleDateChange}
         />
-        </form>
-  )
-}
-}
+        <input
+          type="date"
+          placeholder="Date of booking"
+          value={this.state.date}
+          onChange={this.handleDateChange}
+        />
 
+        <input
+          type="time"
+          placeholder="Time of booking"
+          value={this.state.time}
+          onChange={this.handleTimeChange}
+        />
+        <input
+          type="text"
+          placeholder="Number of people booking"
+          value={this.state.numPeople}
+          onChange={this.handleNumOfPeopleChange}
+        />
+
+        <input type="submit" value="Post" />
+      </form>
+    );
+  }
+}
 
 export default BookingTableForm;
