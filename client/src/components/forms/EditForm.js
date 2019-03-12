91806b0d9a91a8e752b533e5d5780e54a8ba9476
Michael Irwin <mike.irwin1@gmail.com>
2019-03-12 14:43:20 +0000

    edit form updated

--- client/src/components/forms/EditForm.js
+++ client/src/components/forms/EditForm.js
@@ -2,8 +2,10 @@ import React, {useState} from 'react';
 
 const EditForm = (props) => {
     const [booking, setBooking] = useState({
+        name: props.edit.customer.name,
         date: props.edit.date, 
-        time: props.edit.time, 
+        time: props.edit.time,
+        phoneNumber: props.edit.customer.phoneNumber,
         covers: props.edit.numPeople });
 
     const handleChange = (e) => {
@@ -13,6 +15,18 @@ const EditForm = (props) => {
 
     return (
         <form onSubmit = {(e) => {e.preventDefault(); props.handleEdit(booking)}}>
+            <input
+                id = "name"
+                defaultValue = {props.edit.customer.name}
+                onChange  = {handleChange}
+                type = "text"/>
+
+            <input
+                id = "phoneNumber"
+                defaultValue = {props.edit.customer.phoneNumber}
+                onChange  = {handleChange}
+                type = "text"/>
+
             <input 
                 id = "date"
                 defaultValue = {props.edit.date} 
