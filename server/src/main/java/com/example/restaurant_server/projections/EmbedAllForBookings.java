94ff072bd202ad38b4e50147372e86a3bf938538
Thomas Kendrick <tommkendrick@gmail.com>
2019-03-12 12:18:21 +0000

    Fixed broken backend

--- server/src/main/java/com/example/restaurant_server/projections/EmbedAllForBookings.java
+++ server/src/main/java/com/example/restaurant_server/projections/EmbedAllForBookings.java
@@ -8,6 +8,11 @@ import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "embedAllForBookings", types = Booking.class)
 public interface EmbedAllForBookings {
+
+    public Long getId();
+    public String getTime();
+    public String getDate();
+    public int getNumPeople();
     public Customer getCustomer();
     public DiningTable getDiningTable();
 }
