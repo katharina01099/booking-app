29a5d8a8b99abb5c506658b68394083ca513fec8
Thomas Kendrick <tommkendrick@gmail.com>
2019-03-13 00:16:00 +0000

    Changed Tables Header

--- client/src/components/tables/TableList.js
+++ client/src/components/tables/TableList.js
@@ -3,8 +3,13 @@ import TableDetail from "./TableDetail"
 
 const TableList = (props) => {
     if (!props.tables) return null;
+    let tableHeader = <h4>All Tables</h4>
+    let didFilter = false;
     const tables = props.tables.map(table => {
-        if (props.CurrentCovers > table.tableCapacity){ return null} 
+        if (props.CurrentCovers > table.tableCapacity){ 
+            didFilter = true;     
+            return null
+        } 
         return <TableDetail 
           key = {table.id}
           number = {table.tableNum}
@@ -12,10 +17,14 @@ const TableList = (props) => {
           id = {table.id}
           handleTableDynamic = {props.handleTableDynamic}/>
     })
+
+    if (didFilter){
+        tableHeader = <h4> Tables with a capacity of at least {props.CurrentCovers} </h4>
+    }
     return (
         <div className="table-list">
         <form ref={props.ValidateFormRef2}>
-            <h4>All Tables</h4>
+            {tableHeader}
             {tables}
         </form>
         </div>
